bcc649c3d5d485f349ac1beebd06d64740c1522e
FatihSahin <fatihsahin72@gmail.com>
2017-10-21 01:54:45 +0300

    trying to manage dependencies to come up with a client for  a TetFlask-Ready service without whole assistant package

--- TestFlask.Assistant.Core/Models/TestFlaskAssistantContext.cs
+++ TestFlask.Assistant.Core/Models/TestFlaskAssistantContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using TestFlask.Assistant.Config;
 
-namespace TestFlask.Assistant.Models
+namespace TestFlask.Assistant.Core.Models
 {
     public class TestFlaskAssistantContext
     {
---
+++ TestFlask.Assistant.Core/WebApi/TestFlaskMessageHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestFlask.Assistant.Config;
+using TestFlask.Assistant.Models;
+using TestFlask.Models.Context;
+
+namespace TestFlask.Assistant.WebApi
+{
+    /// <summary>
+    /// This class is used for ASP.NET Web API clients. 
+    /// HttpClient that is plugged in with this handler is created like this => HttpClient client = HttpClientFactory.Create(new TestFlaskMessageHandler());
+    /// </summary>
+    public class TestFlaskMessageHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var config = TestFlaskAssistantConfig.Instance;
+            var context = TestFlaskAssistantContext.Current;
+
+            if (config.Enabled && context != null && context.RecordMode)
+            {
+                request.Headers.Add(ContextKeys.ProjectKey, config.Project.Key);
+                request.Headers.Add(ContextKeys.ScenarioNo, context.CurrentScenarioNo.ToString());
+                request.Headers.Add(ContextKeys.TestMode, "Record");
+
+                if (context.OverwriteStepNo > 0)
+                {
+                    request.Headers.Add(ContextKeys.StepNo, context.OverwriteStepNo.ToString());
+                }
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
