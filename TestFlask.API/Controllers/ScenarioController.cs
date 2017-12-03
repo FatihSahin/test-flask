c7d9da570d423a7fe95a27733ef533d7fd4eee94
FatihSahin <fatihsahin72@gmail.com>
2017-12-04 00:35:06 +0300

    clone scenario

--- TestFlask.API/Controllers/ScenarioController.cs
+++ TestFlask.API/Controllers/ScenarioController.cs
@@ -16,10 +16,12 @@ namespace TestFlask.API.Controllers
     public class ScenarioController : ApiController
     {
         private readonly IScenarioRepo scenarioRepo;
+        private readonly ICounterRepo counterRepo;
 
-        public ScenarioController(IScenarioRepo pScenarioRepo)
+        public ScenarioController(IScenarioRepo pScenarioRepo, ICounterRepo pCounterRepo)
         {
             scenarioRepo = pScenarioRepo;
+            counterRepo = pCounterRepo;
         }
 
         [Route("api/scenario/{scenarioNo}")]
@@ -37,7 +39,7 @@ namespace TestFlask.API.Controllers
         [Route("api/scenario")]
         public Scenario Post(Scenario scenario)
         {
-            var result = scenarioRepo.Insert(scenario);
+            var result = scenarioRepo.Insert(scenario, autoGenerateNos: true);
             return result;
         }
 
@@ -49,6 +51,50 @@ namespace TestFlask.API.Controllers
             ApiCache.DeleteScenario(result.ScenarioNo);
 
             return result;
+        }
+
+        [Route("api/scenario/clone")]
+        public Scenario PostCloneScenario(long scenarioNo)
+        {
+            Scenario scenario = scenarioRepo.GetScenario(scenarioNo);
+
+            long oldScenarioNo = scenario.ScenarioNo;
+
+            scenario.Id = null;
+            scenario.ScenarioNo = counterRepo.GetNextCounter("scenario").CounterValue;
+            scenario.ScenarioName = !string.IsNullOrWhiteSpace(scenario.ScenarioName) ? $"{scenario.ScenarioName.Trim()}_CloneOf{oldScenarioNo}" : $"CloneOf{oldScenarioNo}";
+
+            foreach (Step step in scenario.Steps)
+            {
+                long oldStepNo = step.StepNo;
+                step.ScenarioNo = scenario.ScenarioNo;
+                step.CreatedOn = DateTime.UtcNow;
+
+                step.StepNo = counterRepo.GetNextCounter("step").CounterValue;
+                foreach (Invocation invocation in step.Invocations)
+                {
+                    invocation.StepNo = step.StepNo;
+                    invocation.ScenarioNo = step.ScenarioNo;
+                    // Recalculate hash codes
+                    invocation.SignatureHashCode = invocation.GetSignatureHashCode();
+                    invocation.RequestHashCode = invocation.GetRequestHashCode();
+                    invocation.DeepHashCode = invocation.GetDeepHashCode();
+                    invocation.LeafHashCode = invocation.GetLeafHashCode();
+
+                    string oldInstanceHashCode = invocation.InstanceHashCode;
+                    invocation.InstanceHashCode = invocation.GetInvocationInstanceHashCode();
+                    //update parent instance hash code to preserve tree strucuture for new instance hash codes
+                    step.Invocations.Where(s => s.ParentInstanceHashCode == oldInstanceHashCode).Select(i =>
+                    {
+                        i.ParentInstanceHashCode = invocation.InstanceHashCode;
+                        return i;
+                    });
+                }
+            }
+
+            var inserted = scenarioRepo.Insert(scenario, autoGenerateNos: false);
+
+            return inserted;
         }
     }
 }
